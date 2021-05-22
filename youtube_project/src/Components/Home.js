c4bcf99c94146af7906c9664cce9847a0716764f
Jeffrey <jeffreysebastian@pursuit.org>
2021-05-22 12:56:40 -0400

    worked on search bar

--- youtube_project/src/Components/Home.js
+++ youtube_project/src/Components/Home.js
@@ -1,64 +1,90 @@
-import React from "react";
-import './Home.css'
-import axios from 'axios'
-require('dotenv').config()
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+require("dotenv").config();
 
-class Home extends React.Component {
+const Home = () => {
+  const [input, setInput] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
 
-state = { input: '', hasSearched: false, videos: []}
+  const fetchVideos = async () => {
+    try {
+      const res = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/search?q=${input}&key=${process.env.REACT_APP_YOUTUBE_APIKEY}`
+      );
+      // console.log(res.data.items[0].id.videoId);
+      setVideos(res.data.items);
+      debugger
+      console.log(res.data.items)
+    } catch (error) {
+      console.log(error);
+      setVideos([]);
+    }
+  };
+  // useEffect(() => {
+  //   fetchVideos();
+  // }, []);
 
-fetchVideos = async() => {
-  const {input} = this.state
-
-    const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?q=${input}&key=${process.env.REACT_APP_YOUTUBE_APIKEY}`)
-    console.log(res.data.items[0].id.videoId)
-    this.setState({videos: res.data.items})
+  const handleChange = (e) => {
+    setInput(e.target.value);
     // debugger
-}
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setHasSearched(true);
+    fetchVideos();
+    setInput("");
+  };
 
-handleChange = (e) =>{
-  this.setState({input: e.target.value})
-  // debugger
-}
-handleSubmit = (e) =>{
-  e.preventDefault()
-  this.fetchVideos()
+  // searchByKeyword = () =>{
+  //   const results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
 
-}
+  //   for(let i in results.items) {
+  //     let item = results.items[i];
+  //     Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+  //   }
+  // }
 
-// searchByKeyword = () =>{
-//   const results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+  // let search;
+  // if(hasSearched){
+  //   if(videos?.title){
+  //     search = {
 
-//   for(let i in results.items) {
-//     let item = results.items[i];
-//     Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-//   }
-// }
-
-
-  render() {
-      const {input, videos} = this.state
-      const videoSrc = `https://www.youtube.com/embed/${videos[0].id.videoId}`;
-    return (
-      <div>
-        <h2> Search for some videos</h2>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" placeholder="search for a video"></input>
-          <button type="submit">Search</button>
-        </form>
+  //     }
+  //   }
+  // }
+  return (
+    <div>
+      <h2> Search for some videos</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="search for a video"
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+      {hasSearched ? (
         <section id="videos-container">
-        <iframe src={videoSrc} allowFullScreen title='Video player'/>
-        <ul>
-          {videos.map(item =>{
-      
-      return <li key={item.id.videoId}>{item.id.videoId}</li>
-    })}
-    </ul>
-
+          <ul>
+            {videos.map((item) => {
+              const videoSrc = `https://www.youtube.com/embed/${item.id.videoId}`;
+              return (
+                <iframe
+                  src={videoSrc}
+                  allowFullScreen
+                  title="Video player"
+                  key={item.id.videoId}
+                />
+              );
+              // <li key={item.id.videoId}>{item.id.videoId}</li>
+            })}
+          </ul>
         </section>
-      </div>
-    );
-  }
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default Home;
